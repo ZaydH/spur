9618318c0f74c0776d9da1b95593a3cb6f33e6b0
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2020-03-11 06:38:18 -0700

    Updated build.sh script to better log build errors

--- build.sh
+++ build.sh
@@ -7,18 +7,18 @@ BIN_NAME=spur
 
 printf "Starting the build of the $BIN_NAME binary...\n"
 printf "Deleting the \"build\" directory...\n"
-rm -rf build &> /dev/null
+rm -rf build > /dev/null
 
 printf "Running the setup script...\n"
-bash setupdev.sh &> /dev/null
-cd build/Release &> /dev/null
+bash setupdev.sh > /dev/null
+cd build/Release > /dev/null
 
 printf "Making the \"Release\" program...\n"
-make &> /dev/null
+make > /dev/null
 if [ ! -f $BIN_NAME ]; then
    printf "\033[1;31mMAKE FAILED\033[0m\n"
+   exit 1
 fi
 
-cd ../.. &> /dev/null
+cd ../.. > /dev/null
 printf "Build completed.\n"
-
